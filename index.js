6978ed6191ed11554abc52cf92e9d22d0a8753cc
Michael Fahim <michael_fahim@yahoo.com>
2020-11-03 23:29:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 function fetchBooks() {
-
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then(resp => resp.json())
+        // .then(json => console.log(json))
+        .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const main = document.querySelector('main')
+
+    json.forEach(book => {
+        const h3 = document.createElement('h3')
+
+        h3.innerHTML = `<h3>${book.name}</h3>`
+        main.appendChild(h3)
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+    fetchBooks()
+});
